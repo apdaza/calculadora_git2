7af37d002ab9b6172becec757aa81a40592cb03b
apdaza <apdaza@gmail.com>
2021-03-25 10:28:41 -0500

    creada la función suma de la calculadora

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+class Calculadora:
+    def __init__(self):
+        self.valor_1 = 0
+        self.valor_2 = 0
+        self.resultado = 0
+
+    def sumar(self):
+        self.resultado = self.valor_1 + self.valor_2
