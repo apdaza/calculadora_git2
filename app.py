4281162e6f6f48cc7ce0e0f4145fb111b9274f0c
apdaza <apdaza@gmail.com>
2021-03-25 11:07:53 -0500

    operación suma agregada al app flask

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def hola():
     return 'Hola mundo'
 
+@app.route('/suma/<int:valor_1>/<int:valor_2>')
+def suma(valor_1, valor_2):
+    c = Calculadora()
+    c.valor_1 = valor_1
+    c.valor_2 = valor_2
+    c.sumar()
+    return str(c.valor_1) + " + " + str(c.valor_2) + " = " + str(c.resultado)
+
 if __name__ == '__main__':
     app.run(debug = True)
