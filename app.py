7d2610d3d73cee398d8b451dfcd400f2fd502c6f
apdaza <apdaza@gmail.com>
2021-03-29 11:41:02 -0500

    agregada gui calculadora

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from calculadora import Calculadora
 
 app = Flask(__name__)
 
 @app.route('/')
 def hola():
-    return 'Hola mundo'
+    return render_template('calculadora.html')
 
 @app.route('/suma/<int:valor_1>/<int:valor_2>')
 def suma(valor_1, valor_2):
@@ -13,7 +13,7 @@ def suma(valor_1, valor_2):
     c.valor_1 = valor_1
     c.valor_2 = valor_2
     c.sumar()
-    return str(c.valor_1) + " + " + str(c.valor_2) + " = " + str(c.resultado)
+    return str(c.resultado)
 
 @app.route('/resta/<int:valor_1>/<int:valor_2>')
 def resta(valor_1, valor_2):
@@ -21,7 +21,7 @@ def resta(valor_1, valor_2):
     c.valor_1 = valor_1
     c.valor_2 = valor_2
     c.restar()
-    return str(c.valor_1) + " - " + str(c.valor_2) + " = " + str(c.resultado)
+    return str(c.resultado)
 
 @app.route('/multiplicacion/<int:valor_1>/<int:valor_2>')
 def multiplicacion(valor_1, valor_2):
@@ -29,30 +29,33 @@ def multiplicacion(valor_1, valor_2):
     c.valor_1 = valor_1
     c.valor_2 = valor_2
     c.multiplicar()
-    return str(c.valor_1) + " * " + str(c.valor_2) + " = " + str(c.resultado)
+    return str(c.resultado)
 
-@app.route('/division/<int:valor_1>/<int:valor_2>')
 def division(valor_1, valor_2):
     c = Calculadora()
     c.valor_1 = valor_1
     c.valor_2 = valor_2
     c.dividir()
-    return str(c.valor_1) + " / " + str(c.valor_2) + " = " + str(c.resultado)
+    return int(c.resultado)
 
-@app.route('/operacion/<operador>/<int:valor_1>/<int:valor_2>')
+@app.route('/operacion/<operador>/<int:valor_1>/<int:valor_2>', methods = ['POST', 'GET'])
 def operacion(operador, valor_1, valor_2):
-    if operador == "+":
-        resultado =  suma(valor_1, valor_2)
-    elif operador == "-":
-        resultado =  resta(valor_1, valor_2)
-    elif operador == "*":
-        resultado =  multiplicacion(valor_1, valor_2)
-    elif operador == "d":
-        resultado =  division(valor_1, valor_2)
+    if request.method == 'POST':
+        if operador == "sumar":
+            resultado =  suma(valor_1, valor_2)
+        elif operador == "restar":
+            resultado =  resta(valor_1, valor_2)
+        elif operador == "multiplicar":
+            resultado =  multiplicacion(valor_1, valor_2)
+        elif operador == "dividir":
+            resultado =  division(valor_1, valor_2)
+        else:
+            resultado = hola()
+        return render_template('calculadora.html', resultado = resultado)
     else:
-        resultado = hola()
+        return render_template('calculadora.html')
+
     
-    return render_template('calculadora.html', resultado = resultado)
 
 
 if __name__ == '__main__':
